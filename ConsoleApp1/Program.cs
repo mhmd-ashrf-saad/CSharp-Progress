764c07b9216f568efc1933d53fc529cc571eb156
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2025-02-22 14:26:32 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the original string: ");
+string origin = Console.ReadLine();
+Console.Write("Enter the string you want to replace: ");
+string old = Console.ReadLine();
+Console.Write("Enter the new string: ");
+string nw= Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write($"The new string is: {origin.Replace(old, nw)}\n");
+Console.ForegroundColor = ConsoleColor.White;
