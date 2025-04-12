9cefbfa0fb7fe6e8113caaeb327605ddf7ea3fe9
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-04-12 19:10:24 +0200

    Create ReverseArray.cs

---
+++ Reverse dynamic array using for/ReverseArray.cs
@@ -0,0 +1,22 @@
+Console.Write("Enter number of items: ");
+int size=int.Parse(Console.ReadLine()); // size of array
+int[] numbers = new int[size+1]; // initialize array
+for(int i=1;i<=size;i++)// fill the array
+{
+    Console.WriteLine($"Enter element number {i}: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+// original array
+Console.WriteLine("The array you entered:");
+for (int i = 1; i <= size; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+//reversed array
+Console.WriteLine("\nThe Reversed Array:");
+for(int i=size;i>=1;i--)
+{
+    Console.WriteLine(numbers[i]);
+}
